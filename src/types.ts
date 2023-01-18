b8d5d3b318f037f8c8dcb5a0be28f78ac31750e9
jessicayve <yve.jessica@gmail.com>
2023-01-18 14:18:42 -0300

    typescript I

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export const enum Categoria {
+    ACCESSORIES = "Acessórios",
+    CLOTHES_AND_SHOES = "Roupas e calçados",
+    ELECTRONICS = "Eletrônicos"
+}
+
+
+export type TUser = {
+    id:string,
+    email:string,
+    password:string
+
+}
+
+export type TProduct = {
+    id:string, 
+    name:string,
+    price:number,
+    category:string
+}
+
+export type TPurchase = {
+    userId: string,
+    productId: string,
+    quantity: number,
+    totalPrice: number
+}
+

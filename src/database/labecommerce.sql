802dfca3df8cdb4a1a33aab9cc40e2fea8fd3267
jessicayve <yve.jessica@gmail.com>
2023-01-19 15:38:00 -0300

    refactor

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,4 +1,4 @@
--- Active: 1674064914573@@127.0.0.1@3306
+-- Active: 1674139856019@@127.0.0.1@3306
 CREATE TABLE users(
 id TEXT PRIMARY KEY UNIQUE NOT NULL,
 email TEXT UNIQUE NOT NULL,
@@ -30,12 +30,13 @@ CREATE TABLE products (
 
 INSERT INTO products(id, name, price, category)
 VALUES
-('p0001', 'blusa', 10, 'roupa'),
-('p0002', 'vestido', 23, 'roupa'),
-('p0003', 'chinelo', 13, 'calçados'),
-('p0004', 'colar', 12, 'acessórios'),
-('p0005', 'pulseira', 16, 'acessórios');
+('p001', 'blusa', 10, 'roupa'),
+('p002', 'vestido', 23, 'roupa'),
+('p003', 'chinelo', 13, 'calçados'),
+('p004', 'colar', 12, 'acessórios'),
+('p005', 'pulseira', 16, 'acessórios');
 
+DROP TABLE products;
 
 -- Get All Products
 SELECT * FROM products;
@@ -113,7 +114,7 @@ VALUES
 ('pu001',20, 0,'u001'),
 ('pu002',30, 0,'u001'),
 ('pu003',60, 0,'u002'),
-('pu004',30.2, 0,'u002'),
+('pu004',30, 0,'u002'),
 ('pu005',10, 0,'u003'),
 ('pu006',11, 0,'u003');
 
@@ -138,3 +139,34 @@ SELECT * FROM purchases
 INNER JOIN users
 ON purchases.buyer_id = users.id;
 -- WHERE buyer_id ='u001';
+
+
+CREATE TABLE purchases_products(
+ purchase_id TEXT NOT NULL,
+ product_id TEXT NOT NULL,
+ quantity INTEGER NOT NULL,
+ FOREIGN KEY (purchase_id) REFERENCES purchases(id),
+ FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+INSERT INTO purchases_products(purchase_id, product_id, quantity)
+VALUES 
+('pu001', 'p003', 3),
+('pu002', 'p001', 1),
+('pu005', 'p005', 3);
+
+
+DROP TABLE purchases_products;
+
+SELECT * FROM purchases_products;
+
+SELECT 
+products.name,
+purchases_products.product_id AS productId,
+purchases_products.purchase_id AS purchaseId,
+purchases_products.quantity AS quantity
+FROM purchases_products
+INNER JOIN products
+ON purchases_products.product_id = products.id
+INNER JOIN purchases
+ON purchases_products.purchase_id = purchases.id;
